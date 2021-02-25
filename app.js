2432821cfdea86c0e446553a475521212b411742
Dylan Kok <tenkotofu@gmail.com>
2021-02-25 16:58:04 +0800

    Set express for frontend. See 7f16b70

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+// Imports and requires
 const express = require('express')
 const app = express()
+const exphbs  = require('express-handlebars');
+
+// Express options
+app.use(express.static(`public`))
+// app.enable('view cache');
+
+// Handlebars options
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: `views/layouts/`
+}))
+app.set('view engine', 'hbs')
+app.set('views', `views`)
+
+// Webserver port
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Tony')
-})
+// Express routings
+app.get('/', (req, res, next) => {
+  res.render('index', { layout: 'main' });
+});
 
+// Initialise webserver
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
