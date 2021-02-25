7f16b7041282397bbc28a4de85c07483bcdf89b8
Dylan Kok <tenkotofu@gmail.com>
2021-02-25 16:57:43 +0800

    Added frontend

---
+++ public/vendor/makerspace/app.js
@@ -0,0 +1,34 @@
+setInterval(() => {
+    var timestampNow = new Date()
+
+    $("#time-display").text(timestampNow.toLocaleTimeString())
+
+    var curHr = timestampNow.getHours()
+
+    if (curHr < 10) {
+        $("#good-day").html(`<i class="fas fa-moon"></i>&ensp;Closed for the day`);
+    } else if (curHr < 12) {
+        $("#good-day").html(`Good Morning!`);
+    } else if (curHr < 18) {
+        $("#good-day").html(`Good Afternoon!`);
+    } else if (curHr < 21) {
+        $("#good-day").html("Good Evening!");
+    } else if (curHr < 24) {
+        $("#good-day").html(`<i class="fas fa-moon"></i>&ensp;Closed for the day`);
+    }
+}, 1000)
+
+setInterval(() => {
+    $('#input-cardID').val("")
+}, 500)
+
+
+
+$(function () {
+    $('#scanCardModal').on('shown.bs.modal', function () {
+        $('#input-cardID').focus()
+    });
+    $("#scanCardModal").click(function () {
+        $('#input-cardID').focus()
+    });
+});
