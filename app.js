8ce821b5282db40408b8f6e47699bff6edb33155
YXY-Git-Hub <me@yxy.ninja>
2021-02-25 15:31:49 +0800

    🐛FIX: Deployment setting

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
+require("dotenv").config();
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Tony')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`SAC attandance app listening at http://localhost:${port}`)
 })
