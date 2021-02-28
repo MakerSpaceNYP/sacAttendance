5c42e8050d461d3f7bf0b7d6f293ad389b1a55bd
Tony Yu <itsfishpain@gmail.com>
2021-02-28 14:25:11 +0800

    reduced lag from 1300ms to 900 ms

--- app.js
+++ app.js
@@ -33,47 +33,129 @@ const base = new Airtable({apiKey: AIRTABLE_API_KEY}).base(baseId);
 const port = process.env.PORT || 3000
 
 
+// // Retrieve all SAC Info from SAC Information and store in array
+// const sacInfo = (callback) => {
+// /**
+//  * Retrieve all SAC Info from SAC Information and store in array
+//  * @param {Function} callback - Validation Function
+//  */
+
+//   var start = new Date()
+//   var hrstart = process.hrtime()
+
+//   sacInfoObj = {}
+//   base('SAC Information').select({
+//     // Selecting the first 100 records in Grid view:
+//     view: "Grid view"
+//     }).eachPage(function page(records, fetchNextPage) {
+//     // This function (`page`) will get called for each page of records.
+
+//     records.forEach(function(record) {
+//       // Save record into an array
+//       sacInfoObj[record.get('Card ID')] = record.id
+//     });
+    
+
+//     // To fetch the next page of records, call `fetchNextPage`.
+//     // If there are more records, `page` will get called again.
+//     // If there are no more records, `done` will get called.
+//     fetchNextPage();
+
+//     }, function done(err) {
+//         if (err) { console.error(err); return; }
+//         callback()
+//         var end = new Date() - start,
+//         hrend = process.hrtime(hrstart)
+    
+//       console.info('Execution time: %dms', end)
+//       });
+// }
+
+
+// Execution time: 900ms
 // Retrieve all SAC Info from SAC Information and store in array
-const sacInfo = (callback) => {
+const sacInfo = (cardID, callback, err) => {
 /**
  * Retrieve all SAC Info from SAC Information and store in array
  * @param {Function} callback - Validation Function
  */
+  var start = new Date()
+  let count = 0
   sacInfoObj = {}
   base('SAC Information').select({
     // Selecting the first 100 records in Grid view:
-    maxRecords: 3,
     view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
-
     records.forEach(function(record) {
       // Save record into an array
-      sacInfoObj[record.get('Card ID')] = record.id
-    });
-    
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-    }, function done(err) {
-        if (err) { console.error(err); return; }
+      if(record.get('Card ID') == cardID){
+        sacInfoObj[record.get('Card ID')] = record.id
         callback()
-      });
+        var end = new Date() - start
+        console.log('Execution time: %dms', end)
+        return
+      }
+      else{
+        count ++
+      }
+    });
+    if (count >= records.length){
+      err()
+    }
+  })
 }
+
 
 // Express routings
 app.get('/', (req, res) => {
   res.render('index', { layout: 'main' });
 });
 
+
+// //Execution time: 1308ms
+// app.post('/', (req, res) => {
+//   const card_id = req.body.cardID
+//   sacInfo(function(){
+//     // If Card ID can be found in SAC Information
+//     if (card_id in sacInfoObj){
+//       const checkInDateTime = new Date()
+//       const recordId = sacInfoObj[card_id]
+//         base('SAC Time Sheet').create([
+//           {
+//             "fields": {
+//               "Card ID":card_id,
+//               "SAC Name":[recordId],
+//               "Admin Number":[recordId],
+//               "Check In Date-Time": checkInDateTime
+//             }
+//           },
+//         ], function(err) {
+//           if (err) {
+//             console.error(err);
+//             return;
+//           }
+//         });
+//         res.render('index',{
+//           'statusSuccessIn': true,
+//           'timeLogged': checkInDateTime.toLocaleTimeString()
+//         })
+//     }
+//     // If Card ID cannot be found in SAC Information
+//     else{
+//       // Feedback: User Not Found
+//       res.render('index',{
+//         'statusFail': true
+//       })
+//     }
+//   })
+  
+  
+// })
+
 app.post('/', (req, res) => {
-  const card_id = req.body.cardID
-  sacInfo(function(){
-    // If Card ID can be found in SAC Information
-    if (card_id in sacInfoObj){
+    const card_id = req.body.cardID
+    sacInfo(card_id, function(){
       const checkInDateTime = new Date()
       const recordId = sacInfoObj[card_id]
         base('SAC Time Sheet').create([
@@ -95,18 +177,16 @@ app.post('/', (req, res) => {
           'statusSuccessIn': true,
           'timeLogged': checkInDateTime.toLocaleTimeString()
         })
-    }
-    // If Card ID cannot be found in SAC Information
-    else{
+    }, function(){
+      // If Card ID cannot be found in SAC Information
       // Feedback: User Not Found
       res.render('index',{
         'statusFail': true
       })
     }
+    )
   })
   
-  
-})
 
 // Initialise webserver
 app.listen(port, () => {
